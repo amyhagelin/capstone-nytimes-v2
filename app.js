80a7f7d4dda188b68d2e4aff18311075742123e4
amyhagelin <ahagelin@gmail.com>
2017-01-16 22:09:16 -0800

    final functionality with both search options

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 // add header w/ start over button
 // maybe keep search bar visible
 // save items to read for later
+// git remote problem
 
 
 var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 var searchTermArr = [];
+var state = {
+  searchResults: [],
+  favoriteArticles: []
+}
+
+// add to favorites button -> favoriteArticles.push(...)
+// renderArticles(state.favoriteArticles)
+
+/*
+
+-> data-article-id on a button
+-> get data-article-id on click
+-> find the article in the list of the articles
+-> push the article to the array
+
+*/
 
 function getDataFromApi(searchTerm, callback, sort) {
   var query = {
@@ -22,24 +39,67 @@ function getDataFromApi(searchTerm, callback, sort) {
   $.getJSON(url, query, callback);
 }
 
+function renderHome() {
+  var resultElement = 
+  '<div id="start-page">' +
+      '<h1>My NY Times</h1>'+
+      "<p>Input the topics you'd like to read about today, clicking the + button after each one. When you're finished, click Next.</p>" +
+      '<form action="#" class="js-submit-form">' +
+      '<label for="query"></label>' +
+         '<input type="text" class="js-query">'+
+         '<button type="submit">&#10133;</button></br>'+
+       '</form>'+
+       '<button class="next">Next</button>'+
+     '</div>'+
+     '<p class="hide-me">Or search for an article directly...</p>'+
+     '<div id="start-page-search">'+
+       '<form action="#" class="js-search-form">'+
+       '<label for="query"></label>'+
+       '<input type="text" class="js-query">'+
+       '<button type="submit">Search</button>'+
+       '<label><input type="radio" name="article-search-type" id="new-box" value="first_checkbox" required>Show newest first</label>'+
+        '<input type="radio" name="article-search-type" id="relevant-box" value="second_checkbox" required> <label for="cbox2">Show most relevant first</label>'+
+     '</form>'+
+     '</div>';
+      $('#home').html(resultElement);
+
+}
+
+function renderHeader() {
+  var resultElement = '<div><button class="start-over">Start Over</button></div>';
+  $('.header').html(resultElement);
+}
+
 function renderSearchTermButton(searchTerms) {
-  var resultElement;
-  searchTerms.forEach(function(item) {
-    resultElement += '<button class="search-term">' + item + '</button>';
-  })
+  // var resultElement;
+  // searchTerms.forEach(function(item) {
+  //   resultElement += '<button class="search-term">' + item + '</button>';
+  // })
+  // console.log(resultElement)
 	// var resultElement = '';
-	// resultElement = searchTerms.items.reduce(function(acc, item) {
- //      console.log(acc)
- //      return acc += '<button class="search-term">' + item + '</button>';
- //    }, '')
+  // console.log(searchTerms)
+	var resultElement = searchTerms.reduce(function(acc, item) {
+      console.log(acc)
+      return acc += '<button class="search-term">' + item + '</button>';
+    }, '')
 	// resultElement = '<p>Recently Searched: </p>' + resultElement;
-	$('.recently-searched').html(resultElement);
+	$('.search-terms').html(resultElement);
+
 }
 
 // function renderSortButton() {
 // 	var resultElement = '<button class="sort">Sort by most recent</button>'
 // 	$('.recently-searched').html(resultElement);
 // }
+
+// function Animal(name) {
+//   this._fancyName = 'qwe';
+//   this.name = name; 
+// }
+
+// console.log(new Animal('cat'))
+
+
 
 function renderSearchData(data) {
   var resultElement = '';
@@ -61,7 +121,7 @@ function renderSearchData(data) {
   	  }
   	  else {
 	    return acc += '<div><a target="_blank" href="' + item.web_url + '"><h3>' + 
-	    item.headline.main + '</h3></a><button>&#10133;</button><p>' + currentDate + '</p><p>' 
+	    item.headline.main + '</h3></a><button data-article-id="' + item._id + '">&#10133;</button><p>' + currentDate + '</p><p>' 
 	    + item.snippet + '</p></div>'
       }
       // what to do with undefined? another if statement?
@@ -78,8 +138,10 @@ function renderSearchData(data) {
 }
 
 function watchSubmit() {
-  $('.js-search-form').submit(function(e) {
+  $(document).on('submit', '.js-submit-form', function(e) {
     e.preventDefault();
+    $( "#start-page-search" ).hide();
+    $( ".hide-me" ).hide();
     var query = $(this).find('.js-query').val();
     searchTermArr.push(query);
     console.log(searchTermArr);
@@ -88,8 +150,23 @@ function watchSubmit() {
   });
 }
 
+function watchSearchBox() {
+  $(document).on('submit', '.js-search-form', function(e) {
+    console.log('submit')
+    e.preventDefault();
+    $( "#start-page" ).hide();
+    $( "p.hide-me" ).hide();
+    $(function(){renderHeader();});
+    var query = $(this).find('.js-query').val();
+    if($('#new-box').prop('checked')) {
+      getDataFromApi(query, renderSearchData, 'newest');
+    }
+    if($('#relevant-box').prop('checked')) {
+      getDataFromApi(query, renderSearchData);
+    }
+  });
+}
 
-// where do I add this in the process?
 function watchSearchTermButton() {
 	$(document).on('click', 'button.search-term', function(event) { 
 	event.preventDefault();
@@ -100,8 +177,26 @@ function watchSearchTermButton() {
   });
 	}
 
-$(function(){watchSubmit();});
-$(document).on('click', 'button.next', function(event) { 
-    $( "#start-page" ).hide();
-    watchSearchTermButton();
+$( document ).ready(function() {
+        console.log("ready");
+        $(function(){renderHome();});
+        $(function(){watchSubmit();});
+    $(function(){watchSearchBox();});
+    $(document).on('click', 'button.next', function(event) { 
+        $( "#start-page-search" ).hide();
+        $(function(){renderHeader();});
+        $( "#start-page" ).hide();
+        watchSearchTermButton();
+    });
+    $(document).on('click', 'button.start-over', function(event) { 
+        console.log("Start Over was clicked");
+        $('.js-search-results').empty();
+        $('.search-terms').empty();
+        $( ".header" ).hide();
+        renderHome();
+        searchTermArr = [];
+        // watchSearchBox();
+    // how to clear other data?
 });
+});
+
